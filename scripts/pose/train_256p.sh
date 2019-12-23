38fb1bc47d1f397ce9ed6d10132ef615f5943c0f
Tris <tris@tristolliday.com>
2019-12-23 10:18:02 +0000

    Update train_256p.sh

--- scripts/pose/train_256p.sh
+++ scripts/pose/train_256p.sh
@@ -2,6 +2,6 @@ python /content/vid2vid/train.py --name pose2body_256p \
 --dataroot datasets/pose --dataset_mode pose \
 --input_nc 6 --num_D 2 \
 --resize_or_crop randomScaleHeight_and_scaledCrop --loadSize 384 --fineSize 256 \
---gpu_ids 0,1,2,3,4,5,6,7 --batchSize 8 --max_frames_per_gpu 3 \
+--gpu_ids 0 --batchSize 2 --max_frames_per_gpu 3 \
 --niter 5 --niter_decay 5 \
 --no_first_img --n_frames_total 12 --max_t_step 4

a66998935e22b2dca8847b514a242698483ececf
Tris <tris@tristolliday.com>
2019-12-23 11:03:04 +0000

    Update train_g1_256p.sh

--- scripts/pose/train_g1_256p.sh
+++ scripts/pose/train_g1_256p.sh
@@ -1,4 +1,4 @@
-python train.py --name pose2body_256p_g1 \
+python /content/vid2vid/train.py --name pose2body_256p_g1 \
 --dataroot datasets/pose --dataset_mode pose \
 --input_nc 6 --ngf 64 --num_D 2 \
 --resize_or_crop randomScaleHeight_and_scaledCrop --loadSize 384 --fineSize 256 \
